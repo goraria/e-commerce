dd82168da0e3f5ba5e46ab27cb0057f4f0751687
pa106 <zzphananhzz@gmail.com>
2024-12-11 22:33:27 +0700

    payhd

--- server/src/routes/AccountRoute.js
+++ server/src/routes/AccountRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const AccountController = require('../controllers/AccountController');
 const AuthenticationMiddleware = require('../middleware/AuthenticationMiddleware');
 
-// Lấy thông tin tài khoản người dùng
+
 router.get('/get-info', AuthenticationMiddleware, AccountController.getAccountInfo);
 router.put('/set-info', AuthenticationMiddleware, AccountController.setAccountInfo);
 router.post('/forgot-password', AccountController.ForgotPassword);
