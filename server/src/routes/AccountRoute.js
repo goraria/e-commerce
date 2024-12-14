89426760cffcecaca115a6fdf4b7cb35e4a84ae3
pa106 <zzphananhzz@gmail.com>
2024-12-14 09:00:43 +0700

    payhd

--- server/src/routes/AccountRoute.js
+++ server/src/routes/AccountRoute.js
@@ -7,6 +7,6 @@ const AuthenticationMiddleware = require('../middleware/AuthenticationMiddleware
 
 router.get('/get-info', AuthenticationMiddleware, AccountController.getAccountInfo);
 router.put('/set-info', AuthenticationMiddleware, AccountController.setAccountInfo);
-
+router.post('/change-password', AuthenticationMiddleware, AccountController.changePassword);
 
 module.exports = router;
