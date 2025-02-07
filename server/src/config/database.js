7acaf506ec659cfb1740a7fc0efdde1523eb0493
goraria <gorariagroup@gmail.com>
2025-02-07 21:43:18 +0700

    Japtor

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require("sequelize");
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE, 'root', process.env.SQL_PASSWORD, {
     host: 'localhost',
