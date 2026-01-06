73af47a4b615d2351c7473fd9d5eac960166a69f
goraria <gorariagroup@gmail.com>
2026-01-06 09:34:45 +0700

    Update

--- develop/src/components/button/ResponseButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./GradientButton.css";
-
-export const ResponseButton = () => {
-    return (
-        <button className="gradient-button">Send a Response</button>
-    );
-}
